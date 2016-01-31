aef675eb9b62dab6c6dd5902c587f476df126322
Timo Koski <timo.o.koski@gmail.com>
2016-01-31 15:33:47 +0200

    First helloworlds etc

--- src/TestGitEclipse.java
+++ src/TestGitEclipse.java
@@ -2,6 +2,22 @@
 public class TestGitEclipse {
 
 	public static void main(String[] args) {
+		
+		String a = "Hello world!";
+		
+		System.out.println(a);
+		
+		
+		System.out.println(testiFunktio(a));
+		
+	}
+	
+	public static int testiFunktio(String a) {
+			
+		if (a.equals("Hello world!"))
+			return 1;
+		return 0;
+		
 	}
 
 }
